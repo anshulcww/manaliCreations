1ac093c563526bdd4321ed3bddcebae845b5b8b5
Anshul Verma <anshulverma96@gmail.com>
2018-02-25 00:55:47 +0530

    server_connnected

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("hello world");
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 5000;
+http.listen(port, "0.0.0.0", function () {
+
+    console.log('up and running');
+});
+
+
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
